dfbbf2e1f3e0cc26acb075ac240a8bcc56b2cf9c
l.kaesberg <l.kaesberg@stud.uni-goettingen.de>
2022-12-06 11:42:02 +0100

    feat: solution day 6

---
+++ 2022/solutions/solution06.cpp
@@ -0,0 +1,28 @@
+#include <map>
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main() {
+    string line;
+    ifstream input("../2022/inputs/input06.txt");
+    int numberOfDistinctCharacters = 14;
+    if (input.is_open()) {
+        while (getline(input, line)) {
+            for (int i = numberOfDistinctCharacters - 1; i < line.length(); ++i) {
+                set<char> temp;
+                for (int j = 0; j < numberOfDistinctCharacters; ++j) {
+                    temp.insert(line[i - j]);
+                }
+                if (temp.size() == numberOfDistinctCharacters) {
+                    cout << i + 1;
+                    break;
+                }
+            }
+        }
+        input.close();
+    } else cout << "Unable to open file";
+}
